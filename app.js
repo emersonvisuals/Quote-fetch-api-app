1263776f3888b1aa242d8baf3b90c256d61070cc
Thomas Emerson <emersonvisuals@gmail.com>
2022-08-16 10:06:40 +1000

    spinner added

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const btn = document.querySelector('.dice img');
 const quote = document.querySelector('.quoteDetails');
+const quoteContainer = document.querySelector('.quote');
 const headingNumber = document.querySelector('.headingNumber');
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
+const spinner = document.querySelector('.loader');
 
 btn.addEventListener('click', () => {
+    spinner.classList.add('active');
+    quoteContainer.classList.add('active');
     fetch('https://api.adviceslip.com/advice')
     .then(response => response.json())
     .then(data => {
         quote.innerText = data.slip.advice
         headingNumber.innerText = `advice #${data.slip.id}`
-    })
+        spinner.classList.remove('active');
+        quoteContainer.classList.remove('active');
+    });
+
+
 })
